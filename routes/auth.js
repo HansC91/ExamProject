72eadf0142da1866ff4a17b56a3cdc397fd0d85e
Hans Christian Gustafsson <gustafsson.hansc@gmail.com>
2023-06-02 10:04:42 +0200

    Auth tested and complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,3 @@
-// API CA
 var express = require('express');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
@@ -46,25 +45,25 @@ router.post('/login', function(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
   if (!username || !password ) {
-    return res.status(404).json({ message: 'Please provide both username and password'});
+    return res.status(401).json({ Unauthorized: 'Please provide both username and password'});
   }
 
   userService.getOneByName(username).then((user) => {
     if (!user) {
-      return res.status(404).json({ message: 'No user with that username exists.' });
+      return res.status(401).json({ Unauthorized: 'No user with that username exists.' });
     }
     
     crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
       if (err) { return next(err); }
       if (hashedPassword.length !== user.encryptedPassword.length || !crypto.timingSafeEqual(user.encryptedPassword, hashedPassword)) {
         
-        return res.status(401).json({ message: 'Incorrect login details'});
+        return res.status(401).json({ Unauthorized: 'Incorrect login details'});
       }
 
       const token = jwt.sign({ username: username.id, id: user.id }, process.env.TOKEN_SECRET, { expiresIn: "2h" });
       res.cookie('jwt', token);
 
-      res.status(200).json({ "result": 'You are logged in', id: user.id, username: user.username, token: token});
+      res.status(200).json({ Successful: 'You are logged in', id: user.id, username: user.username, token: token});
     });  
   }).catch((err) => {
     return next(err);
@@ -72,20 +71,32 @@ router.post('/login', function(req, res, next) {
 });
 
 
-router.post('/signup', jsonParser, async (req, res, next) => {
-  const {name, email, password } = req.body;
-  if (name == null) {
-    return res.jsend.fail({ name: "Must fill in name"});
+router.post('/signup', async (req, res, next) => {
+  const {username, firstname, lastname, email, password} = req.body;
+  if (!username || !email) {
+    return res.status(409).json({ Conflict: "Username and email needs to be filled in"});
   }
-  if (email == null) {
-    return res.jsend.fail({ email: "Must fill in email"});
+  if (!firstname || !lastname) {
+    return res.status(409).json({ Conflict: 'Please enter first- and lastname'});
   }
-  if (password == null) {
-    return res.jsend.fail({ password: "Must enter password"});
+  if (password.length < 6 ) {
+    return res.status(409).json({ Conflict: "Must enter password of 6 characters or longer"});
   }
-  var user = await userService.getOneByName(email);
-  if (user != null) {
-    return res.jsend.fail({ email: "Email allready in use"});
+  var uniqueUsername = await userService.getOneByName(username);
+  if (uniqueUsername !== null && username === uniqueUsername.username) {
+    return res.status(409).json({ Conflict: "Username allready in use, try another one"})
+  }
+
+  var emailAccounts = await userService.getAllEmail(email);
+  var count = 0;
+  
+  for (var i = 0; i < emailAccounts.length; i++) {
+    if(emailAccounts[i].email === email) {
+      count++;
+    }
+  }
+  if (count >= 4) {
+    return res.status(409).json({ Conflict: "Email allready in use on 4 family members"});
   }
   var salt = crypto.randomBytes(16);
   crypto.pbkdf2(password, salt, 310000, 32, 'sha256', async (err, hashedPassword) => {
@@ -93,8 +104,8 @@ router.post('/signup', jsonParser, async (req, res, next) => {
       return next(err);
     }
     try {
-      await userService.create(name, email, hashedPassword, salt);
-      res.jsend.success({ result: "Account created succesfully"});
+      await userService.create(username, firstname, lastname, email, hashedPassword, salt);
+      res.status(200).json({ Successful: "Account created succesfully"});
     } catch (error) {
       next(error);
     }
